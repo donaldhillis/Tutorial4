79f46f0ee7171192154852d9e4438fb9b60e205b
Donald Hillis <donaldhillis@hotmail.co.nz>
2019-08-16 22:39:48 +1200

    Steps 1 to 9 completed

---
+++ src/test/java/Tut4/TestCalc.java
@@ -0,0 +1,15 @@
+package Tut4;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestCalc 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAdd()
+    {
+        assert(12 == (new Calc().add(5, 7)));
+    }
+}
